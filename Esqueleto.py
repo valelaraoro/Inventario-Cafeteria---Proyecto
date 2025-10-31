1058bb66fdc4f88a04115ad4d17951c9ff89f863
sebastian <al07170560@tecmilenio.mx>
2025-10-31 12:29:13 -0600

    codigo

--- Esqueleto.py
+++ Esqueleto.py
@@ -1,3 +1,17 @@
 #------------MONITOREO DE INVENTARIO DE CAFETERÍA-------------
 #----------------MENÚ DE CAFETERÍA-----------------------
 #----------------------------------------------------------
+# print("bienvenido a a cafeteria")
+# num_pedido =print("este es el menu: \n burrito\n chilaquiles \n molletes\n")
+
+# burrito = 30
+# chilaquiles = 10
+# molletes = 10
+# num_pedido = (input("que le gustaria ordenar: "))
+# print ("esto ordeno:", num_pedido)
+# if num_pedido == "burrito":
+#     print ("hay 30 burritos en existencias")
+# elif num_pedido == "chilaquiles":
+#     print("hay 10 chilaquiles en existencia")
+# elif num_pedido == "molletes":
+#     print("hay 15 molletes en existencias")
