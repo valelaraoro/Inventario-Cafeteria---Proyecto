2b50628aea7f8e92522dbd01481e43801ff587e6
Valentina <AL07181318@tecmilenio.mx>
2025-11-25 12:41:22 -0600

    Inventario final

--- inventraio-cafeteria-final.py
+++ inventraio-cafeteria-final.py
@@ -1,7 +1,7 @@
 print(" ----------MENÚ CAFETERÍA---------- ")
 Productos = []
 
-# Pedir cuántos productos se registrarán
+
 while True:
     try:
         Num_productos = int(input("¿Cuántos productos se registrarán? "))
@@ -19,9 +19,6 @@ def menu():
     print("5. Salir")
 
 
-# ---------------------------
-#   OPCIÓN 1: Registrar productos
-# ---------------------------
 def opcion1(Productos):
     print("\n----- Registro de productos -----")
 
@@ -58,9 +55,6 @@ def opcion1(Productos):
     print("\nRegistro completado correctamente.")
 
 
-# ---------------------------
-#   OPCIÓN 2: Mostrar inventario
-# ---------------------------
 def opcion2():
     if len(Productos) == 0:
         print("No hay productos registrados.")
@@ -74,9 +68,6 @@ def opcion2():
     print("-------------------------\n")
 
 
-# ---------------------------
-#   OPCIÓN 3: Registrar ventas
-# ---------------------------
 def opcion3(Productos):
     if len(Productos) == 0:
         print("No hay productos registrados.")
@@ -115,11 +106,9 @@ def opcion3(Productos):
     print("Venta registrada con éxito.")
 
 
-# ---------------------------
-#   Cálculo de ingresos
-# ---------------------------
+
 def calcular_consumo(Productos):
-    Resultados = []
+    Resultados = [] 
 
     for p in Productos:
         ingresos = p["Vendido"] * p["Precio"]
@@ -133,9 +122,6 @@ def calcular_consumo(Productos):
     return Resultados
 
 
-# ---------------------------
-#   OPCIÓN 4: Reporte final
-# ---------------------------
 def opcion4(Productos):
     if len(Productos) == 0:
         print("No hay datos para guardar.")
@@ -157,9 +143,6 @@ def opcion4(Productos):
         print("Error al guardar el archivo.")
 
 
-# ---------------------------
-#   BUCLE PRINCIPAL
-# ---------------------------
 while True:
     menu()
     Opcion = input("Selecciona una opción: ")
