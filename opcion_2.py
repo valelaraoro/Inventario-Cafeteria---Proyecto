117af4de4fe9f4488a20e52589137447a34db57e
abelpee <al07143067@tecmilenio.mx>
2025-11-25 11:48:23 -0600

    codigo con la opcion 2

--- opcion_2.py
+++ opcion_2.py
@@ -1,8 +1,45 @@
-# Lista de productos de la cafetería
+print(" ----------MENÚ CAFETERÍA---------- ")
+
+# Aquí estará la lista de productos
 Productos = ["Burrito de asado", "Burrito de chicharron", "Chilaquiles", "Pan dulce", "Santa clara chocolate"]
 
-# Función Opción 2: mostrar productos
-def opcion2(Productos):
-    print("\nMENÚ DE PRODUCTOS")
-    for i, producto in enumerate(Productos, start=1):
-        print(f"{i}. {producto}")
+def menu():
+    print("\n       MENÚ PRINCIPAL      ")
+    print("1. Registrar productos")
+    print("2. Mostrar inventario")
+    print("3. Realizar ventas")
+    print("4. Reporte Final")
+    print("5. Salir")
+
+# -------------------------
+#        OPCIÓN 2
+# -------------------------
+def opcion2():
+    print("\n--- MENÚ DE PRODUCTOS ---")
+    
+    contador = 1
+    for producto in Productos:
+        print(f"{contador}. {producto}")
+        contador += 1
+
+    print("-------------------------\n")
+
+
+# Bucle principal
+while True:
+    menu()
+    Opcion = input("Selecciona una opción: ")
+
+    if Opcion == "1":
+        print("Opción 1 aún no programada")
+    elif Opcion == "2":
+        opcion2()
+    elif Opcion == "3":
+        print("Opción 3 aún no programada")
+    elif Opcion == "4":
+        print("Opción 4 aún no programada")
+    elif Opcion == "5":
+        print("Saliendo del sistema...")
+        break
+    else:
+        print("Opción no válida")
