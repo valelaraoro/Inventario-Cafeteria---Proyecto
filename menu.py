6957f491498c33ebf942ac00d713e4eec4c1bafa
Ventura <venturaorozco7532@gmail.com>
2025-11-27 11:57:31 -0600

    Menu

---
+++ menu.py
@@ -0,0 +1,8 @@
+menu = (
+    {"nombre": "Chilaquiles", "Precio": "25": "Cantidad": 20},
+    {"nombre": "Molletes", "Precio": "65": "Cantidad": 15},
+    {"nombre": "Pan dulce", "Precio": "15": "Cantidad": 30}
+    {"nombre": "Vaso de fruta", "Precio": "40": "Cantidad": 30}
+    {"nombre": "Elote en vaso", "Precio": "35": "Cantidad": 15}
+    {"nombre": "Nachos con queso", "Precio": "45": "Cantidad": 20}
+    )
