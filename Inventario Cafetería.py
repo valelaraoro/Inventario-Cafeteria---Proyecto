4577f27f4aef4bf048b6ccd48097892037a64d1a
Ventura <venturaorozco7532@gmail.com>
2025-11-28 11:02:27 -0600

    Inventario cafetería casi terminado

--- Inventario Cafetería.py
+++ Inventario Cafetería.py
@@ -26,9 +26,143 @@ def registrar_productos():
         except:
           print("Ingresa un número válido")
 
-for i in range(n)
-    print(f"\")
-#HOla
+for i in range(n):
+    print(f"\nProducto{i+1}:")
+    nombre = input("Nombre: ")
+
+    while True:
+        try:
+            precio = float(input("Precio: "))
+            break
+        except:
+            print("Ingresa un número válido")
+
+    while True:
+        try:
+            cantidad = int(input("Cantidad: "))
+            break
+        except:
+            print("Ingresa un número válido")
+    
+    producto = {"nombre": nombre, "precio": precio, "cantidad": cantidad, "vendido": 0}
+    Productos.append(producto)
+    print("\nRegistro completado.")
+
+    def mostrar_inventario():
+        if len(Productos) == 0:
+            print("\nNo hay productos registrados")
+            return
+        print("\n--- Inventario ---")
+        i = 1 
+        for p in Productos:
+            print(f"{i}. {p['nombre']}| Precio: {p['precio']} | Cantidad: {p['cantidad']} | Vendido: {p['vendido']})")
+            i += 1
+    
+    def registrar_venta():
+        if len(Productos) == 0:
+            print("\nNo hay productos registrados.")
+            return
+        print("\n--- Registrar venta ---")
+        i = 1
+        for p in Productos:
+            print(f"{i}. {p['nombre']} (Stock: {p['cantidad']})")
+            i += 1
+
+        while True:
+            try:
+                opcion = int(input("Selecciona el número del producto: "))
+                if 1 <= opcion <= len(Productos):
+                    break
+                else:
+                    print("Oppción Inválida.")
+            except:
+                print("Ingresa un número válido.")
+            
+        producto = Productos[opcion-1]
+
+        while True:
+            try:
+                cantidad = int(input("Cantidad vendida: "))
+                if 0 <= cantidad <= producto["cantidad"]:
+                    break
+                else:
+                    print(f"No hay suficiente stock. Disponible: {producto['cantidad']}")
+            except:
+                print("Ingresa un número válido")
+                 
+                producto["cantidad"] -= cantidad
+                producto["vendido"] += cantidad
+    print(f"\nVenta registrada: {cantidad} unidades de {producto['nombre']}")
+
+    def producto_mas_vendido():
+        if len(Productos) == 0:
+            print("\nNo hay productos registrados.")
+            return
+        
+        mas_vendido = Productos[0]
+        for p in Productos:
+            if p["vendido"] > mas_vendido["vendido"]:
+                mas_vendido = p
+            if mas_vendido["vendido"]== 0:
+                print("\nAún no se han registrado ventas.")
+            else:
+                print("\n--- Producto más vendido ---")
+                print(f"Nombre: {mas_vendido['nombre']}")
+                print(f"Unidades vendidas: {mas_vendido['vendido']}")
+                print(f"Ingresos: {mas_vendido['vendido'] * mas_vendido['precio']}")
+        
+        def reporte_final():
+            if len(Productos)== 0:
+                print("\nNo hay productos registrados. No se puede generar reporte final")
+                return
+            print("\n--- REPORTE FINAL ---")
+            total_ventas = 0
+            total_ingresos = 0
+            mas_vendido = Productos [0]
+
+        i = 1
+        for p in Productos:
+                ingresos = p['vendido'] * p['precio']
+                total_ventas += p['vendido']
+                total_ingresos += ingresos
+                print(f"{i}. {p['nombre']} | Vendido: {p['vendido']} | inventario restante: {p['cantidad']} | Ingresos: ${ingresos}")
+                
+                if p['vendido'] > mas_vendido['vendido']:
+                    mas_vendido = p
+                i += 1
+                print("\n--- RESUMEN ---")
+                print(f"Total de productos vendidos: {total_ventas}")
+                print(f"Ingresos totales: ${total_ingresos}")
+                if mas_vendido['vendido']> 0:
+                    print(f"Producto más vendido: {mas_vendido['nombre']}({mas_vendido['vendido']} unidades)")
+                else:
+                    print("Aún no se han registrado ventas.")
+    while True:
+        menu()
+        opcion = input("Selecciona una opción: ")
+        
+        if opcion == "1":
+         registrar_productos()
+        elif opcion == "2":
+            mostrar_inventario()
+        elif opcion == "3":
+            registrar_venta()
+        elif opcion == "4":
+            producto_mas_vendido()
+        elif opcion == "5":
+            reporte_final()
+        elif opcion == "6":
+            print("Saliendo del programa. ¡Hasta luego!")
+            break
+        
+        else:
+            print("Opción no válida. Intenta de nuevo.")
+                
+
+
+
+            
+
 
 
 
